b38d6e035c61991563c91a469438739cf35b3e30
Taeyeon <Taeyeon@192.168.0.115>
2022-01-13 14:44:55 +0900

    VO, View

---
+++ src/mvc/view/ScoreView.java
@@ -0,0 +1,16 @@
+package mvc.view;
+
+import java.util.ArrayList;
+import mvc.vo.StudentVO;
+
+public class ScoreView {
+	
+	private ArrayList<StudentVO> studentArrList;
+	
+	public ScoreView(ArrayList<StudentVO> studentArrList) {
+		this.studentArrList = studentArrList;
+	}
+
+	
+	
+}
---
+++ src/mvc/vo/StudentVO.java
@@ -0,0 +1,77 @@
+package mvc.vo;
+
+public class StudentVO {
+
+	private String name;
+	private int stuId;
+	private int kor, eng, math, science, history;
+	
+	public StudentVO(String name, int stuId, int kor, int eng, int math, int science, int history) {
+		this.name = name;
+		this.stuId = stuId;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.science = science;
+		this.history = history;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStuId() {
+		return stuId;
+	}
+
+	public void setStuId(int stuId) {
+		this.stuId = stuId;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getScience() {
+		return science;
+	}
+
+	public void setScience(int science) {
+		this.science = science;
+	}
+
+	public int getHistory() {
+		return history;
+	}
+
+	public void setHistory(int history) {
+		this.history = history;
+	}
+	
+	
+	
+}
